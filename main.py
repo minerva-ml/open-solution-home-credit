3872a949c0730fb289018d1fb3db69613c7df5d4
Jakub <czakon.jakub@gmail.com>
2018-07-10 18:35:31 +0200

    added second level models (#126)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+
 from src.pipeline_manager import PipelineManager
 
 pipeline_manager = PipelineManager()
@@ -60,17 +61,21 @@ def evaluate_predict(pipeline_name, submit_predictions, dev_mode):
 
 @main.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
+@click.option('-l', '--model_level', help='level of modeling first or second are available', default='first',
+              required=True)
 @click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
-def train_evaluate_cv(pipeline_name, dev_mode):
-    pipeline_manager.train_evaluate_cv(pipeline_name, dev_mode)
+def train_evaluate_cv(pipeline_name, model_level, dev_mode):
+    pipeline_manager.train_evaluate_cv(pipeline_name, model_level, dev_mode)
 
 
 @main.command()
 @click.option('-p', '--pipeline_name', help='pipeline to be trained', required=True)
+@click.option('-l', '--model_level', help='level of modeling first or second are available', default='first',
+              required=True)
 @click.option('-s', '--submit_predictions', help='submit predictions if true', is_flag=True, required=False)
 @click.option('-d', '--dev_mode', help='if true only a small sample of data will be used', is_flag=True, required=False)
-def train_evaluate_predict_cv(pipeline_name, submit_predictions, dev_mode):
-    pipeline_manager.train_evaluate_predict_cv(pipeline_name, dev_mode, submit_predictions)
+def train_evaluate_predict_cv(pipeline_name, model_level, dev_mode, submit_predictions):
+    pipeline_manager.train_evaluate_predict_cv(pipeline_name, model_level, dev_mode, submit_predictions)
 
 
 if __name__ == "__main__":
